49de1fc19232558e588e494b2477764c47d249e4
Gleb B <104177228+Gump0@users.noreply.github.com>
2026-04-16 19:28:29 +0000

    finished chapter 2

--- src/Ch01-The Basics of Exception Handling/01_06/begin/basics.cpp
+++ src/Ch01-The Basics of Exception Handling/01_06/begin/basics.cpp
@@ -4,27 +4,41 @@
 
 using namespace std;
 
-const string readConfigFile(const string& filename) 
+const string readConfigFile(const string &filename)
 {
     ifstream file(filename);
+    if (!file.good())
+    {
+        // MY OLD CODE COMMENTED
+        // cerr << "ERROR WHEN ATTEMPTING TO OPEN CONFIG FILE" << endl;
+        throw runtime_error("Failed to open configuration file");
+    }
 
     string line;
     string text;
-    while (getline(file, line)) 
+
+    try
     {
-        text += line;
-        text += '\n';
+        while (getline(file, line))
+        {
+            text += line;
+            text += '\n';
+        }
+    }
+    catch (const exception &e)
+    {
+        cerr << "ERROR: " << e.what() << endl;
+        throw e;
     }
 
     return text;
 }
 
-int main() 
-{    
+int main()
+{
     cout << "Reading configuration:" << endl;
     string text = readConfigFile("configuration.txt");
     cout << text;
-    
+
     return 0;
 }
-
