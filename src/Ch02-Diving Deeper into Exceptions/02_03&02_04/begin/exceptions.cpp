b6d6d187a907853824b38aba986f25be805fd39a
Gleb B <104177228+Gump0@users.noreply.github.com>
2026-04-17 00:50:32 +0000

    2.3 & 2.4

---
+++ src/Ch02-Diving Deeper into Exceptions/02_03&02_04/begin/exceptions.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+enum ErrorType
+{
+    None,
+    Disk,
+    Permission,
+    FileIO
+};
+
+class DiskAccessException : public std::runtime_error
+{
+public:
+    DiskAccessException(const std::string &msg) : std::runtime_error(msg) {}
+};
+
+class FilePermissionException : public std::runtime_error
+{
+public:
+    FilePermissionException(const std::string &msg) : std::runtime_error(msg) {}
+};
+
+class FileIOException : public std::runtime_error
+{
+public:
+    FileIOException(const std::string &msg) : std::runtime_error(msg) {}
+};
+
+void triggerException(ErrorType error)
+{
+    switch (error)
+    {
+    case ErrorType::Disk:
+        throw DiskAccessException("Disk access error.");
+    case ErrorType::Permission:
+        throw FilePermissionException("File access error.");
+    case ErrorType::FileIO:
+        throw FileIOException("File IO error.");
+    default:
+        break; // No exception thrown
+    }
+}
+
+int main()
+{
+    try
+    {
+        triggerException(ErrorType::FileIO);
+    }
+    catch (const DiskAccessException &e)
+    {
+        std::cerr << "DiskAccessException: " << e.what() << std::endl;
+    }
+    catch (const FilePermissionException &e)
+    {
+        std::cerr << "FilePermissionException: " << e.what() << std::endl;
+    }
+    catch (const FileIOException &e)
+    {
+        std::cerr << "FileIOException: " << e.what() << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "std::exception " << e.what() << std::endl;
+    }
+    catch (...) // HAS to be last, or else it will throw a compile error.
+    {
+        std::cerr << "An unknown exception occurred." << std::endl;
+    }
+    return 0;
+}
